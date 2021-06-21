7c8c33a54fe578d25bba43841836315f6fc22e78
Igorase <garik.sushik@ya.ru>
2021-06-21 23:48:18 +0500

    - base.html adding - styles adding - fonts adding - visual initial

--- landing_site/mafia_bot/urls.py
+++ landing_site/mafia_bot/urls.py
@@ -2,5 +2,7 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path("", main_sample, name="main_sample")
+    path("", main_sample, name="main_sample"),
+    path("second/", second, name="second"),
+    path("third/", third, name="third")
 ]
--- landing_site/mafia_bot/views.py
+++ landing_site/mafia_bot/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+menu = [{"title": "Главная", "url_name": "main_sample"},
+        {"title": "second", "url_name": "second"},
+        {"title": "third", "url_name": "third"}]
+
 
 def main_sample(request):
-    return HttpResponse("главная страница")
+    return render(request, "landing/main_sample.html", context={"menu": menu, "title": "один"})
+
+
+def second(request):
+    return render(request, "landing/second.html", context={"menu": menu, "title": "два"})
+
+
+def third(request):
+    return render(request, "landing/third.html", context={"menu": menu, "title": "три"})
 
 
 def pageNotFound(request, exception):
