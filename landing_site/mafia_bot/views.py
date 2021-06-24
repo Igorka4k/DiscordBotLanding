9236fba1c441ecde95da486a55c317abe8d4c37c
Igorase <garik.sushik@ya.ru>
2021-06-25 02:46:25 +0500

    little bugfix

--- landing_site/mafia_bot/views.py
+++ landing_site/mafia_bot/views.py
@@ -1,23 +1,24 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-menu = [{"title": "Главная", "url_name": "main_sample"},
+menu = [{"title": "main", "url_name": "main_sample"},
         {"title": "second", "url_name": "second"},
         {"title": "third", "url_name": "third"}]
 
 
 def main_sample(request):
-    context = {"menu": menu, "title": "один", "modes": ["common", "extreme", "special", "random"]}
+    modes = ["common", "extreme", "special", "random", "wild forest", "night off"]
+    context = {"menu": menu, "title": "Modes", "modes": modes}
     return render(request, "mafia_bot/main_sample.html", context=context)
 
 
 def second(request):
-    context = {"menu": menu, "title": "два"}
+    context = {"menu": menu, "title": "Second"}
     return render(request, "mafia_bot/second.html", context=context)
 
 
 def third(request):
-    context = {"menu": menu, "title": "три"}
+    context = {"menu": menu, "title": "Third"}
     return render(request, "mafia_bot/third.html", context=context)
 
 
